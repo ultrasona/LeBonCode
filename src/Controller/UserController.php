afef4ff13e8982505a3f781011666ed7e5ecfea3
Sophie Da Silva <sophie.dasilva@youboox.fr>
2021-05-09 14:49:15 +0200

    starting user

---
+++ src/Controller/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    /**
+    * @Route("/register", name="user_regiset", methods={"POST"})
+    */
+
+}
